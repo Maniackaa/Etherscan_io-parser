62ef19a457e42af50d6d6dea1b5998fe2aa7b502
Maniackaa <maniac_kaa@mail.ru>
2023-05-28 15:45:49 +0700

    Fix bug

--- database/db_func.py
+++ database/db_func.py
@@ -69,7 +69,7 @@ async def get_last_hour_transaction(lower_target, time_period=60) -> list[(str, 
     async with async_session() as session:
         query = select(
             Transaction.token, func.count(
-                Transaction.token), Transaction.token_adress).group_by(
+                Transaction.token), func.max(Transaction.token_adress)).group_by(
             Transaction.token_adress).order_by(
             func.count(Transaction.token).desc()).where(
             (Transaction.addet_time > datetime.datetime.now()
